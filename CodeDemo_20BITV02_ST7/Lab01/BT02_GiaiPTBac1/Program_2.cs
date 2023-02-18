3293ef67bb4ec8e0b03b5752d11cd35985f10f03
Hien Luong <hyhien@gmail.com>
2023-02-18 10:41:41 +0700

    Update code

---
+++ CodeDemo_20BITV02_ST7/Lab01/BT02_GiaiPTBac1/Program_2.cs
@@ -0,0 +1,31 @@
+﻿// Lab 01 - BT02 - Giải pt bậc 1: ax + b = 0
+Console.WriteLine("Giai phuong trinh bac 1!");
+try
+{
+    Console.Write("He so a: ");
+    int heSoA = int.Parse(Console.ReadLine());
+    Console.Write("He so b: ");
+    int heSoB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"PT {heSoA}*x + {heSoB} = 0");
+    if (heSoA == 0)
+    {
+        if (heSoB == 0)
+        {
+            Console.WriteLine("Phuong trinh co vo so nghiem");
+        }
+        else
+        {
+            Console.WriteLine("PT vo nghiem");
+        }
+    }
+    else
+    {
+        double nghiem = -heSoB / heSoA;
+        Console.WriteLine($"PT co nghiem: x={nghiem}");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Lỗi: " + ex.Message);
+}
---
+++ CodeDemo_20BITV02_ST7/Lab01/BT03_Mang/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Viết chương trình nhập một mảng số nguyên, Sau đó thực hiện các yêu cầu:
+o Xuất ra tổng của mảng.
+o Xuất ra số nguyên lớn nhất trong mảng.
+o Xuất ra số nguyên nhỏ nhất trong mảng.
+*/
+Console.Write("Nhập số lượng phần tử: ");
+int soLuong = int.Parse(Console.ReadLine());
+
+// Khai báo mảng động
+List<int> myList = new List<int>();
+
+//duyệt qua từng phần và nhập
+for (int i = 0; i < soLuong; i++)
+{
+    Console.Write($"Phần tử thứ {i}: ");
+    var tmp = int.Parse(Console.ReadLine());
+
+    myList.Add(tmp);//thêm phần tử vào mảng
+}
+
+//Xuất
+Console.WriteLine("Mảng vừa nhập:");
+Console.WriteLine(string.Join(", ", myList));
+
+Console.WriteLine($"Tổng: {myList.Sum()}");
+Console.WriteLine(myList.Max());
+Console.WriteLine(myList.Min());
